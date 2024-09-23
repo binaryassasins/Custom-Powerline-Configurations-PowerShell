0d9e0678eb7df2c89ed787f0ffaff09192b03f42
Syafiq Asyraf <82915997+binaryassasins@users.noreply.github.com>
2024-09-23 16:29:01 +0800

    Updated run.bat with corrected PowerShell commands

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ echo "In order for terminal to work properly, please ensure that you have instal
 echo "Running Powerline Installation/Configuration"
 
 powershell -Command "Install-Module -Name Terminal-Icons -RequiredVersion 0.9.0"
-powershell -Command "winget install JanDeDobbeleer.OhMyPosh -s winget; winget install Neovim.Neovim; $profilePath = '.\Microsoft.PowerShell_profile.ps1'; if (Test-Path $profilePath) { Copy-Item -Path $profilePath -Destination '$env:USERPROFILE\Documents\PowerShell'; Write-Host 'Profile replaced successfully.' } else { Write-Host 'Please ensure that you have installed "Windows Terminal" from Microsoft Store (https://apps.microsoft.com/detail/9n0dx20hk701?hl=en-US&gl=US)'; } Copy-Item -Path .\kushalxmod.omp.json -Destination '$env:POSH_THEMES_PATH'"
+powershell -Command "winget install JanDeDobbeleer.OhMyPosh -s winget; Import-Module PowerShellGet; Install-Module -Name Terminal-Icons -RequiredVersion 0.9.0; winget install Neovim.Neovim; $profilePath = '.\Microsoft.PowerShell_profile.ps1'; if (Test-Path $profilePath) { Copy-Item -Path $profilePath -Destination '$env:USERPROFILE\Documents\PowerShell'; Write-Host 'Profile replaced successfully.' } else { Write-Host 'Please ensure that you have installed "Windows Terminal" from Microsoft Store (https://apps.microsoft.com/detail/9n0dx20hk701?hl=en-US&gl=US)'; } Copy-Item -Path .\kushalxmod.omp.json -Destination '$env:POSH_THEMES_PATH'"
 
 echo "Installation Completed! Restart Your Terminal..."
 echo "Press any key to exit."
